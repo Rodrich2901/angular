00fe01ec671f6d5a4f9b8b937eb4aa7b1fecba4a
Rodrigo Beltran <rodrich.bb@gmail.com>
2018-11-28 08:16:22 -0400

    primer commit angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Alumno } from './models/alumno';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'registro-angular-simple';
+  alumnoArray: Alumno[] = [
+    {id:1, nombre_completo: 'Rodrigo Beltrán', identidad: '4285973'},
+    {id:2, nombre_completo: 'Monica Torrez', identidad: '3216547'},
+    {id:3, nombre_completo: 'Jaime Veliz', identidad: '9876543'},
+  ];
+
+  selectedAlumno: Alumno = {id:0,nombre_completo:'',identidad:''};
+
+  openForEdit(Alumno:Alumno): void{
+    this.selectedAlumno = Alumno;
+  }
+
+  addOrEdit(): void{
+    if(this.selectedAlumno.id === 0)
+    {
+      this.selectedAlumno.id = this.alumnoArray.length + 1;
+      this.alumnoArray.push(this.selectedAlumno);
+    }
+    this.selectedAlumno = {id:0, nombre_completo:'',identidad:''};
+  }
+  delete(): void{
+    if(confirm('Estas seguro de borrar el registro'))
+    {
+      this.alumnoArray = this.alumnoArray.filter(x => x != this.selectedAlumno);
+      this.selectedAlumno = {id:0,nombre_completo:'',identidad:''};
+    }
+  }
 }
